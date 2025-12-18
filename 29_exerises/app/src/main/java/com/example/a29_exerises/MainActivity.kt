80524e5d0638a36ddebecb89b32aa91e906427af
Svetlana Rudova <149309473+rudessa@users.noreply.github.com>
2025-12-19 03:03:15 +0800

    Gift

---
+++ 29_exerises/app/src/main/java/com/example/a29_exerises/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.a29_exerises
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btnTest)
+        btn.setOnClickListener {
+            Toast.makeText(this, "Кнопка нажата!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
